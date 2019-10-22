990760cabf910c040e1a6613de8be7e936978886
Berdych <fgs76100@gmail.com>
2019-10-22 15:02:12 +0800

    change version

--- RegisterProfileEditor/__version__.py
+++ RegisterProfileEditor/__version__.py
@@ -1,11 +1,12 @@
 # __version__.py
 
-__version__ = '0.1.0'
+__version__ = '0.1.1'
 
 
 # Revisions
 # 0.0.1 first release
 # 0.1.0 change scheme to dark mode
+# 0.1.1 fix register analyzer bug
 
 
 
