d6aae8ff13c10581252dba6fb698ef0bed41d4ce
Berdych <fgs76100@gmail.com>
2019-10-22 21:21:39 +0800

    0.1.2 revisions

--- RegisterProfileEditor/__version__.py
+++ RegisterProfileEditor/__version__.py
@@ -1,13 +1,16 @@
 # __version__.py
 
-__version__ = '0.1.1'
+__version__ = '0.1.2'
 
 
 # Revisions
 # 0.0.1 first release
 # 0.1.0 change scheme to dark mode
 # 0.1.1 fix register analyzer bug
-
+# 0.1.2 // 1. ignore public column in HTMLWriter.
+#       // 2. RegisterProfileReader now parsing correct information
+#       // 3. remove some useless information in RegisterProfileWriter
+#       // 4. remove current useless column in register column
 
 
 
