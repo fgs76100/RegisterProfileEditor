c65d6e6efc16fcc9fa45ec009c1a4f2537d74abb
Berdych <fgs76100@gmail.com>
2019-10-25 22:46:47 +0800

    revisions 0.2.0

--- RegisterProfileEditor/__version__.py
+++ RegisterProfileEditor/__version__.py
@@ -13,8 +13,10 @@ __version__ = '0.1.4'
 #       // 4. remove current useless column in register column
 # 0.1.3 fix BlockView copy bug
 # 0.1.4 // 1. fix HTMLWriter info bug
-#       // 2. fix textEdit shifte bug
-
+#       // 2. fix textEdit Key_Shift bug
+# 0.2.0 // 1. add offset shifter to register view
+#       // 2. add Key_Delete or Key_D press event to field view for clearing values
+#       // 3. now always save table without asking
 
 
 
