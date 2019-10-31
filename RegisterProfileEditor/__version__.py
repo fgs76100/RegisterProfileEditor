a5373a6f7384ae0aad6f6ce620654b39e6620bce
Berdych <fgs76100@gmail.com>
2019-11-01 00:31:29 +0800

    0.2.1 release

--- RegisterProfileEditor/__version__.py
+++ RegisterProfileEditor/__version__.py
@@ -1,6 +1,6 @@
 # __version__.py
 
-__version__ = '0.2.0'
+__version__ = '0.2.1'
 
 
 # Revisions
@@ -14,10 +14,14 @@ __version__ = '0.2.0'
 # 0.1.3 fix BlockView copy bug
 # 0.1.4 // 1. fix HTMLWriter info bug
 #       // 2. fix textEdit Key_Shift bug
+#
 # 0.2.0 // 1. add offset shifter to register view
 #       // 2. add Key_Delete or Key_D press event to field view for clearing values
 #       // 3. now always save table without asking
-
-
+#
+# 0.2.1 // 1. more table auto save
+#       // 2. fix BlockView shiftBy bug
+#       // 3. resize AnalyzerView table when more detail clicked
+#       // 4. add analyzer menu to reload address
 
 
